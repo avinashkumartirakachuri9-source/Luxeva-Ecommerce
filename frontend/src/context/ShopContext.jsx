06ba22d01c2149356a784a3108578ae981043eae
Avinash Kumar <avinashkumartirakachuri9@gmail.com>
2026-07-06 18:17:07 +0530

    Added Placeorder Page and Functionality

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
@@ -11,6 +12,8 @@ const ShopContextProvider = (props) => {
   const [showSearch, setShowSearch] = useState(false);
 
   const [cartItems, setCartItems] = useState({});
+
+  const navigate=useNavigate();
 
   const addToCart = async (itemId, size) => {
     if (!size) {
@@ -49,7 +52,29 @@ const ShopContextProvider = (props) => {
     return totalCount;
   }
 
+  const updateQuantity=async(itemId,size,quantity)=>{
+    let cartData=structuredClone(cartItems)
+    cartData[itemId][size]=quantity;
+    setCartItems(cartData);
+  }
 
+
+  const getCartAmount=()=>{
+    let totalAmount=0;
+    for (const items in cartItems){
+      let itemInfo=products.find((product)=>product._id===items)
+      for(const item in cartItems[items]){
+        try {
+          if(cartItems[items][item]>0){
+            totalAmount+=itemInfo.price*cartItems[items][item]
+          }
+        } catch (error) {
+          
+        }
+      }
+    }
+    return totalAmount
+  }
   const value = {
     products,
     currency,
@@ -60,7 +85,10 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
-    getCartCount
+    getCartCount,
+    updateQuantity,
+    getCartAmount,
+    navigate
   };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
